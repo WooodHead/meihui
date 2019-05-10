fe2a1296d8939d79c394a7666692f12488b9ab48
gansiqiu <1223720954@qq.com>
2019-05-10 10:39:35 +0800

    添加中英文切换

--- app/public/js/src/config.js
+++ app/public/js/src/config.js
@@ -52,3 +52,41 @@ var config = {
     },
     default_profile: "../public/images/default_profile.jpg"
 }
+
+var lang = document.cookie.split("=")[1];           //当前浏览器语言
+
+$(document).ready(function() {
+    var baseUrl = new String();
+    if(window.location.href.indexOf("&locale=") > 0){   //已经进行过一次语言转化
+        baseUrl =  window.location.href.split("&locale")[0];
+    }else if(window.location.href.indexOf("?locale=") > 0){
+        baseUrl =  window.location.href.split("?locale")[0];
+    }else{
+        baseUrl = window.location.href;
+    }
+
+
+    if(lang == "zh-cn"){
+        $(".lang_zh").addClass('active');
+        $(".lang_en").removeClass('active');
+    }else if(lang == "en-us"){
+        $(".lang_en").addClass('active');
+        $(".lang_zh").removeClass('active');
+    }
+
+    $(".lang_zh").click(function(event) {
+        if (baseUrl.indexOf("?") > 0) {    //链接上？后面有参数
+            window.location.href = baseUrl + "&locale=zh-CN";
+        } else {
+            window.location.href = baseUrl + "?locale=zh-CN";
+        }
+    });
+
+    $(".lang_en").click(function(event) {
+        if (baseUrl.indexOf("?") > 0) {    //链接上？后面有参数
+            window.location.href = baseUrl + "&locale=en-US";
+        } else {
+            window.location.href = baseUrl + "?locale=en-US";
+        }
+    });
+});
